6647ff4832b50138a5dad9603032b5dc3d90e5a9
Jayanth337 <146432702+Jayanth337@users.noreply.github.com>
2024-01-30 14:03:44 +0530

    Add files via upload
    updated html and js files

--- data.js
+++ data.js
@@ -1,33 +1,39 @@
 function userdata(username) {
+    // Fetch user data
     fetch(`https://api.github.com/users/${username}`)
         .then(response => response.json())
         .then(data => {
             console.log(data);
             document.getElementById('userData').innerHTML = `
-            <img style="  height: 200px; width: 200px; padding:50px ;
-  margin: 0;
-  display: flex;
-  align-items: center; 
-  justify-content: center;" src="${data.avatar_url}" alt="Profile Picture" width="150" >    
-            <h1  style="text-align:center; font-size: 40px; text-decoration: underline">Login id:-${data.login}</h1>
-                <p style="text-align:center;">Data Name:${data.name}</p>
-                <p style="text-align:center;">Data Bio:${data.bio}</p>
-                <p style="text-align:center;">Followers: ${data.followers}</p>
-                <p style="text-align:center;">Following: ${data.following}</p>
-                <p style="text align: center;">Your Repositories:</p>
+                <div style="text-align:center;">
+                    <img src="${data.avatar_url}" alt="Profile Picture" width="200" style="border-radius: 50%;">
+                    <h1 style="font-size: 24px; text-decoration: underline;">Login id: ${data.login}</h1>
+                    <p>Data Name: ${data.name}</p>
+                    <p>Data Bio: ${data.bio}</p>
+                    <p>Followers: ${data.followers}</p>
+                    <p>Following: ${data.following}</p>
+                    <p>Your Repositories:</p>
+                </div>
             `;
         })
-        fetch(`https://api.github.com/users/${username}/repos`)
-            .then(response => response.json())
-            .then(data => {
+        .then(() => {
+            return fetch(`https://api.github.com/users/${username}/repos`);
+        })
+        .then(response => response.json())
+        .then(data => {
             console.log(data);
-            for(var i=0;i<=data.length;i++){
+            if (data.length > 0) {
+                data.forEach(repo => {
+                    document.getElementById('userData').innerHTML += `
+                        <p id="reposs" style="text-align:center; font-size: 18px;">${repo.name}</p>
+                    `;
+                });
+            } else {
                 document.getElementById('userData').innerHTML += `
-                <p style="text-align:center;">"${data[i].name}\n"</p>
+                    <p id="reposs" style="text-align:center; font-size: 18px;">No repositories found.</p>
                 `;
             }
         })
-
         .catch(error => {
             console.log('Error:', error);
         });
